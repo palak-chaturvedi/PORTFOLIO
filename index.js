0720e54dac2704fa97af0cc7a9a4b4d4b2eed002
Palak <palakchaturvedi2843@gmail.com>
2021-08-19 22:57:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// The place I saw this effect:
+// https://angle2.agency/
+
+// Red Staper made a NICE tutorial about this effect! You should check it out!
+// https://www.youtube.com/watch?v=LgiadQQM6mo&t=5s
+
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleWindowResize);
+
+const spansSlow = document.querySelectorAll('.spanSlow');
+const spansFast = document.querySelectorAll('.spanFast');
+
+let width = window.innerWidth;
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width/2) - 1;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  spansSlow.forEach((span) => {
+    span.style.transform = `translate(${speedSlow}px)`;
+  });
+  spansFast.forEach((span) => {
+    span.style.transform = `translate(${speedFast}px)`
+  })
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
+$(function(){
+  $().timelinr({
+    orientation: 	'vertical',
+    issuesSpeed: 	300,
+    datesSpeed: 	100,
+    arrowKeys: 		'true',
+    startAt:		3
+  })
+});
